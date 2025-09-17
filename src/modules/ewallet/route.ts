049971ce667eed131de584b75e5e534faae65777
Kien Tran <tkien.ba@gmail.com>
2025-09-17 20:16:23 +0700

    fix: clean code

--- src/modules/ewallet/route.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance, FastifyPluginOptions } from 'fastify';
-
-export default (fastify: FastifyInstance, opts: FastifyPluginOptions, done: () => void) => {
-  fastify.register(import('@ewallet/funds/routes'), {
-    prefix: '/funds',
-  });
-
-  done();
-};

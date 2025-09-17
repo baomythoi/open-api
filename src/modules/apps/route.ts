e33f4ade13f60dd457265cbd848c19aa78555b7d
Kien Tran <tkien.ba@gmail.com>
2025-09-17 20:32:14 +0700

    fix: clean code

--- src/modules/apps/route.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyInstance, FastifyPluginOptions } from 'fastify';
-
-export default (fastify: FastifyInstance, opts: FastifyPluginOptions, done: () => void) => {
-  fastify.register(import('@apps/gsale/route'), {
-    prefix: '/gsale'
-  })
-
-  done();
-};

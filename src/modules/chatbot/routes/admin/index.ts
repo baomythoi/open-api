bc2802e72556f1e7baf1474921c30c830e6d61ef
Kien Tran <tkien.ba@gmail.com>
2025-12-23 17:13:27 +0700

    feat(routes): remove scenarios routes from admin module

--- src/modules/chatbot/routes/admin/index.ts
+++ src/modules/chatbot/routes/admin/index.ts
@@ -2,7 +2,6 @@ import { FastifyInstance, FastifyPluginOptions } from 'fastify';
 
 // routes
 import GeneralsRoutes from '@chatbot/routes/admin/generals.route';
-import ScenariosRoutes from '@chatbot/routes/admin/scenarios.route';
 import UserAdminRoutes from '@authentication/routes/user-admin.route';
 import UsersRoutes from '@chatbot/routes/admin/users.route';
 import PackagesRoutes from '@chatbot/routes/admin/packages.route';
@@ -12,10 +11,6 @@ import ConversationRoutes from '@chatbot/routes/admin/conversations.route';
 export default (fastify: FastifyInstance, opts: FastifyPluginOptions, done: () => void) => {
   fastify.register(GeneralsRoutes, {
     prefix: '/generals',
-  });
-
-  fastify.register(ScenariosRoutes, {
-    prefix: '/scenarios',
   });
 
   fastify.register(UserAdminRoutes, {
